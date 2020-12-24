dbf40a128979bc547587af6214b89de9cdb7a46c
kenchen <4915185-kenchen0625@users.noreply.gitlab.com>
2020-12-24 21:05:35 +0800

    Feat: add cors support

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
